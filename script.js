5ca80e03417fc4e5bb0a24eaa7e5018510ad706e
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-03-01 14:28:12 -0800

    finished all changes for Pixal Art Project

---
+++ script.js
@@ -0,0 +1,47 @@
+function createTable() {
+  var num_rows = document.querySelector('#rows').value;
+  var num_cols = document.querySelector('#cols').value;
+  var theader = '<table id="pixel-container">\n';
+  var tbody = '';
+
+  for (var i = 0; i < num_rows; i++) {
+    tbody += '<tr type="row">';
+    for (var j = 0; j < num_cols; j++) {
+      tbody += '<td class="pixel">';
+      tbody += '</td>'
+    }
+    tbody += '</tr>\n';
+  }
+  var tfooter = '</table>';
+  document.querySelector('#pixel-container').innerHTML = theader + tbody + tfooter;
+  revealPalette(true)
+}
+
+
+function revealPalette(show) {
+
+  if (show) {
+    document.querySelector('#palette').style.visibility = 'visible';
+  } else{
+    document.querySelector('#palette').style.visibility = 'hidden';
+  }
+
+}
+
+var colorPickedCurrently = 'black'
+
+document.querySelector("#palette").addEventListener("click", pickColor);
+
+function pickColor(event) {
+  colorPickedCurrently = event.target.style.backgroundColor
+  document.querySelector('#colorPicked').style.backgroundColor = colorPickedCurrently
+}
+
+
+document.querySelector("#pixel-container").addEventListener("mouseover", paintColor);
+
+function paintColor(event) {
+  if (event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = colorPickedCurrently
+  }
+}
